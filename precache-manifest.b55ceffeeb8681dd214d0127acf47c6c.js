b3b2d5f6181f3972859c669e3c5dcd337a563724
mia <sansolmom@gmail.com>
2019-10-13 12:53:34 +0900

    Updates

---
+++ precache-manifest.b55ceffeeb8681dd214d0127acf47c6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff548fa07e956e478301e96de64f3b2",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "54960a2a6ce35a4eae16",
+    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "9c63e566ee8166e9431f",
+    "url": "/movie_app_2019/static/js/2.6d27e71f.chunk.js"
+  },
+  {
+    "revision": "54960a2a6ce35a4eae16",
+    "url": "/movie_app_2019/static/js/main.6f000243.chunk.js"
+  },
+  {
+    "revision": "420037a5e08d7d6458d7",
+    "url": "/movie_app_2019/static/js/runtime-main.6a961451.js"
+  }
+]);
